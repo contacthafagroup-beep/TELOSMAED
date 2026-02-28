954a83a71a8d14d8e8df508d4bd953cfce71c4f2
mekbib-tilahun <tilahunmekbib345@gmail.com>
2026-02-28 02:57:09 -0800

    fix: remove NextAuth SessionProvider causing 500 errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter, Crimson_Text, Playfair_Display } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
-import SessionProvider from '@/components/providers/SessionProvider'
 import { Header } from '@/components/layout/header'
 import { Footer } from '@/components/layout/footer'
 import { ReadingProgress } from '@/components/ui/reading-progress'
@@ -58,19 +57,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${crimsonText.variable} ${playfairDisplay.variable} font-sans antialiased`}>
-        <SessionProvider>
-          <ThemeProvider>
-            <ReadingProgress />
-            <div className="min-h-screen flex flex-col">
-              <Header />
-              <main className="flex-1">
-                {children}
-              </main>
-              <Footer />
-            </div>
-            <QuickActions />
-          </ThemeProvider>
-        </SessionProvider>
+        <ThemeProvider>
+          <ReadingProgress />
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">
+              {children}
+            </main>
+            <Footer />
+          </div>
+          <QuickActions />
+        </ThemeProvider>
       </body>
     </html>
   )
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { signIn } from 'next-auth/react'
 import Link from 'next/link'
 
 export default function RegisterPage() {
